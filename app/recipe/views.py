cb812fb703cd07b22c0ed3b24365ee052f0596ba
AitorPo <aitorpoquetgin@gmail.com>
2021-09-30 14:00:02 +0200

    Added list ingredient feature.

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,11 @@
 from rest_framework import viewsets, mixins
 # line above ables us to get acces to the CRUD functions
-# thanks to generic viwsets and mixins DRF features
+# thanks to generic viwsets and mixins DRF features such as
+# create, list, retrieve...
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 from recipe import serializers
 
@@ -18,10 +19,26 @@ class TagViewSet(viewsets.GenericViewSet,
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
 
+    # We override this mixins.ListModelMixin feature getting
+    # tags associated to the user who made the request
     def get_queryset(self):
         """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
+    # We override this mixins.CreateModelMixin feature to be able
+    # to create a new tag associated to the user who made the request
     def perform_create(self, serializer):
         """Create a new tag"""
         serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Manage ingredients in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
